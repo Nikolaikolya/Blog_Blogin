f17bd82cbe5031d516b50e1b4f556550fdb31e10
Nikolaikolya <starcrafters@ya.ru>
2020-02-16 22:56:25 +0700

    Доделанно адаптивное меню.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.nav__gumb-wrap').onclick = function(){
+    document.querySelector('.nav__gumb').classList.toggle('nav__gumb--active');
+    document.querySelector('.nav').classList.toggle('active');
+}
